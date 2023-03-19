d5e471766d8b9f5599f3a648b0db4ca596a4712d
felaxd <f.kulas@sharep.io>
2023-03-19 14:22:48 +0100

    Added logging to user change endpoints

--- users/services.py
+++ users/services.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
+from typing import Dict, Any
 
+from django.contrib.admin.models import LogEntry, CHANGE
+from django.contrib.contenttypes.models import ContentType
 from django.db import transaction
+from django.forms import model_to_dict
 
 from users.models import User
 
@@ -9,9 +13,25 @@ from users.models import User
 class UserService:
     @staticmethod
     @transaction.atomic()
-    def update_user(user: User, **kwargs) -> User:
+    def update_user(user: User, **kwargs: Dict[str, Any]) -> User:
         """Updates user details"""
-        User.objects.filter(id=user.id).update(**kwargs)
-        user.refresh_from_db()
-        user.full_clean()
+        # serialize input so kwargs has only User object fields
+        user_model_fields = [field.name for field in User._meta.fields]
+        if kwargs := {k: kwargs[k] for k in user_model_fields if k in kwargs.keys()}:
+            pre_change_details = model_to_dict(user, fields=user_model_fields)
+            User.objects.filter(id=user.id).update(**kwargs)
+            user.refresh_from_db()
+            user.full_clean()
+
+            post_change_details = model_to_dict(user, fields=user_model_fields)
+            changed = [field for field, value in pre_change_details.items() if value != post_change_details[field]]
+            if changed:
+                LogEntry.objects.log_action(
+                    user_id=user.id,
+                    content_type_id=ContentType.objects.get_for_model(user).pk,
+                    object_id=user.pk,
+                    object_repr=user.__str__(),
+                    action_flag=CHANGE,
+                    change_message=[{"changed": {"fields": changed}}],
+                )
         return user
