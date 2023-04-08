c8be755991b2f40b44ae622345310f8d56748273
felaxd <f.kulas@sharep.io>
2023-04-08 15:28:22 +0200

    Added Schedule models

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from schedule.models import Schedule, ScheduleBlock, LecturerScheduleBlockThrough
+
+
 # Register your models here.
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "progress", "created_at")
+    list_filter = ("status",)
+    search_fields = ("name",)
+
+
+class LecturerScheduleBlockThroughInline(admin.TabularInline):
+    model = LecturerScheduleBlockThrough
+    extra = 0
+
+
+@admin.register(ScheduleBlock)
+class ScheduleBlockAdmin(admin.ModelAdmin):
+    list_display = ("course_name", "type", "start", "end", "get_groups", "get_lecturers", "created_at")
+    list_filter = ("groups", "lecturers", "rooms")
+    search_fields = ("course_name", "groups__name", "lecturers__first_name", "lecturers__last_name", "rooms__name")
+    filter_horizontal = ("groups", "rooms")
+    inlines = (LecturerScheduleBlockThroughInline,)
+
+    def get_groups(self, obj: ScheduleBlock) -> str:
+        return ", ".join(obj.groups.values_list("name", flat=True))
+
+    get_groups.short_description = "groups"
+
+    def get_lecturers(self, obj: ScheduleBlock) -> str:
+        return ", ".join([f"{lecturer}" for lecturer in obj.lecturers.all()])
+
+    get_lecturers.short_description = "lecturers"
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import Group
 from django.db import models
+from common.models import BaseDatabaseModel, TimestampMixin
+from courses.models import Course
+from lecturers.models import Lecturer
+from rooms.models import Room
+
 
 # Create your models here.
+class Schedule(BaseDatabaseModel, TimestampMixin):
+    name = models.CharField(max_length=128)
+    status = models.CharField(max_length=32, default="NEW")
+
+    file = models.FileField(upload_to="schedule/uploads/")
+    progress = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ("-created_at",)
+
+
+class ScheduleBlock(BaseDatabaseModel, TimestampMixin):
+    course_name = models.CharField(max_length=128)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, related_name="schedule_blocks", null=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    type = models.CharField(max_length=32)
+    lecturers = models.ManyToManyField(
+        Lecturer, related_name="schedule_blocks", through="LecturerScheduleBlockThrough", blank=True
+    )
+    groups = models.ManyToManyField(Group, related_name="schedule_blocks")
+    rooms = models.ManyToManyField(Room, related_name="schedule_blocks", blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.start.date()} / {self.course_name}"
+
+    class Meta:
+        ordering = ("-created_at", "start")
+
+
+class LecturerScheduleBlockThrough(BaseDatabaseModel, TimestampMixin):
+    schedule_block = models.ForeignKey(ScheduleBlock, on_delete=models.CASCADE)
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.lecturer}{f' / {self.room.name}' if self.room else ''}"
