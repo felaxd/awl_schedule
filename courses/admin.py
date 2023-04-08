a5bb91cc3c4b0e4ec5e8d3727c3c415e1ba558cf
felaxd <f.kulas@sharep.io>
2023-04-08 12:14:02 +0200

    Added Course model and related relations

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from courses.models import Course, LecturerCourseThrough, GroupCourseThrough
+
+
+class GroupCourseThroughInline(admin.TabularInline):
+    model = GroupCourseThrough
+    extra = 0
+
+
+class LecturerCourseThroughInline(admin.TabularInline):
+    model = LecturerCourseThrough
+    extra = 0
+
+
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = (GroupCourseThroughInline, LecturerCourseThroughInline)
+    search_fields = ("name",)
+    list_filter = ("lecturers", "groups")
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import Group
 from django.db import models
 
+from common.models import BaseDatabaseModel, TimestampMixin
+from lecturers.models import Lecturer
+
+
 # Create your models here.
+class Course(BaseDatabaseModel, TimestampMixin):
+    name = models.CharField(max_length=128, blank=False)
+    groups = models.ManyToManyField(Group, through="GroupCourseThrough", related_name="courses")
+    lecturers = models.ManyToManyField(Lecturer, through="LecturerCourseThrough", related_name="courses")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class GroupCourseThrough(BaseDatabaseModel, TimestampMixin):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    lecture_hours = models.IntegerField(default=0)
+    exercises_hours = models.IntegerField(default=0)
+    laboratory_hours = models.IntegerField(default=0)
+    seminary_hours = models.IntegerField(default=0)
+    project_hours = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.group.name
+
+
+class LecturerCourseThrough(BaseDatabaseModel, TimestampMixin):
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    lecture_hours = models.IntegerField(default=0)
+    exercises_hours = models.IntegerField(default=0)
+    laboratory_hours = models.IntegerField(default=0)
+    seminary_hours = models.IntegerField(default=0)
+    project_hours = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"{self.lecturer} / {self.group}"
