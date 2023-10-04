d168837b35621456b720ec1b4ae8a2e3309dd4a1
felaxd <f.kulas@sharep.io>
2023-10-04 15:56:47 +0200

    Api call error fix

--- awl_frontend/src/services/schedule/ScheduleService.jsx
+++ awl_frontend/src/services/schedule/ScheduleService.jsx
@@ -3,14 +3,14 @@ import {config} from "../config";
 export async function getSchedule(data) {
     let queryParams = ""
     console.log(data)
-    if (data.groups) {
-        queryParams += "&groups=" + data.groups.join("&groups=") + "&"
+    if (data.groups?.length > 0) {
+        queryParams += "groups=" + data.groups.join("&groups=") + "&"
     }
-    if (data.lecturers) {
-        queryParams += "&lecturers=" + data.lecturers.join("&lecturers=") + "&"
+    if (data.lecturers?.length > 0) {
+        queryParams += "lecturers=" + data.lecturers.join("&lecturers=") + "&"
     }
-    if (data.rooms) {
-        queryParams += "&rooms=" + data.rooms.join("&rooms=") + "&"
+    if (data.rooms?.length > 0) {
+        queryParams += "rooms=" + data.rooms.join("&rooms=") + "&"
     }
     queryParams = queryParams.replace(/&$/, '');
     return fetch(`${config.API_URL}/schedule/?${queryParams}`)
