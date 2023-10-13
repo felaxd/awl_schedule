a638dd32ff2e11170da0b38d1769a8b4d5af48ff
felaxd <f.kulas@sharep.io>
2023-10-13 15:47:13 +0200

    Added link to administration site

--- awl_frontend/src/components/topbar/Topbar.jsx
+++ awl_frontend/src/components/topbar/Topbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./topbar.css";
 import {Link} from "react-router-dom";
+import {config} from "../../services/config";
+import {Button} from "@mui/material";
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <Link to="/"><img className="logo" src="/logo.png" alt="AWL" /></Link>
+            <Link className="topbar-login-container" to={config.ADMIN_SITE_URL}>
+                <Button id="topbar-login-button">Zaloguj</Button>
+            </Link>
         </div>
     );
 }
--- awl_frontend/src/services/config.jsx
+++ awl_frontend/src/services/config.jsx
@@ -1,3 +1,4 @@
 export const config = {
     API_URL: 'http://ziemowit.ddns.net:8000/api',
+    ADMIN_SITE_URL: 'http://ziemowit.ddns.net:8000/admin/',
 }
