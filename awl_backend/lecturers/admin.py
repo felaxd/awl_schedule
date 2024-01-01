f712ab7782a9b82ddbec599a39bef38bc4b74182
felaxd <f.kulas@sharep.io>
2024-01-01 13:18:42 +0100

    Django admin fixes for lecturers list

--- awl_backend/lecturers/admin.py
+++ awl_backend/lecturers/admin.py
@@ -7,9 +7,12 @@ from lecturers.models import Lecturer
 @admin.register(Lecturer)
 class LecturerAdmin(admin.ModelAdmin):
     list_display = (
-        "first_name",
         "last_name",
+        "_first_name",
         "title",
         "job_position",
     )
     search_fields = ("first_name", "last_name", "contact_email")
+
+    def _first_name(self, obj: Lecturer) -> str:
+        return obj.first_name or "-"
